49e81460f79e6431e43752ce4d88f7dace9dc8c5
fauder <canik.burak@gmail.com>
2025-02-05 13:52:19 +0300

    🔁 Engine/Graphics/InternalTextures: Create utility function to create single texel textures and use it

--- Engine/Engine/Graphics/InternalTextures.cpp
+++ Engine/Engine/Graphics/InternalTextures.cpp
@@ -21,42 +21,60 @@ namespace Engine
 		return TEXTURE_MAP.find( name )->second;
 	}
 
-	void InternalTextures::Initialize()
+	Texture* InternalTextures::CreateSingleTexelTexture( const Color4& texel, const std::string& name,
+														 std::optional< Texture::ImportSettings > import_settings )
 	{
+		return CreateSingleTexelTexture( std::array< unsigned char, 4 >
+										 {
+											 unsigned char( texel.R() * 255.0f ),
+											 unsigned char( texel.G() * 255.0f ),
+											 unsigned char( texel.B() * 255.0f ),
+											 unsigned char( texel.A() * 255.0f )
+										 },
+										 name,
+										 import_settings );
+	}
+
+	Texture* InternalTextures::CreateSingleTexelTexture( const std::array< unsigned char, 4 > texel, const std::string& name, 
+														 std::optional< Texture::ImportSettings > import_settings )
+	{
+		if( not import_settings.has_value() )
 		{
-			const unsigned char normal_map_texel[] = { 127, 127, 255, 255 };
-			TEXTURE_MAP.try_emplace( "Normal Map", AssetDatabase< Texture >::CreateAssetFromMemory( "Default Normal Map",
-																									reinterpret_cast< const std::byte* >( &normal_map_texel ),
-																									1,
-																									true, // => using raw data instead of file contents.
-																									Texture::ImportSettings
-																									{
-																										.wrap_u           = Texture::Wrapping::ClampToEdge,
-																										.wrap_v           = Texture::Wrapping::ClampToEdge,
-																										.min_filter       = Texture::Filtering::Nearest,
-																										.mag_filter       = Texture::Filtering::Nearest,
-																										.flip_vertically  = false,
-																										.generate_mipmaps = false,
-																										.format           = Texture::Format::RGBA
-																									} ) );
+			import_settings = Texture::ImportSettings
+			{
+				.wrap_u           = Texture::Wrapping::ClampToEdge,
+				.wrap_v           = Texture::Wrapping::ClampToEdge,
+				.min_filter       = Texture::Filtering::Nearest,
+				.mag_filter       = Texture::Filtering::Nearest,
+				.flip_vertically  = false,
+				.generate_mipmaps = false
+			};
 		}
 
-		{
-			const unsigned char texel[] = { 255, 255, 255, 255 };
-			TEXTURE_MAP.try_emplace( "White", AssetDatabase< Texture >::CreateAssetFromMemory( "White",
-																							   reinterpret_cast< const std::byte* >( &texel ),
-																							   1,
-																							   true, // => using raw data instead of file contents.
-																							   Texture::ImportSettings
-																							   {
-																								   .wrap_u           = Texture::Wrapping::ClampToEdge,
-																								   .wrap_v           = Texture::Wrapping::ClampToEdge,
-																								   .min_filter       = Texture::Filtering::Nearest,
-																								   .mag_filter       = Texture::Filtering::Nearest,
-																								   .flip_vertically  = false,
-																								   .generate_mipmaps = false
-																							   } ) );
-		}
+		return AssetDatabase< Texture >::CreateAssetFromMemory( name,
+																reinterpret_cast< const std::byte* >( &texel ),
+																1,
+																true, // => using raw data instead of file contents.
+																*import_settings );
+	}
+
+	void InternalTextures::Initialize()
+	{
+		TEXTURE_MAP.try_emplace( "White",		CreateSingleTexelTexture( Color4::White(),		"White" ) );
+		TEXTURE_MAP.try_emplace( "Black",		CreateSingleTexelTexture( Color4::Black(),		"Black" ) );
+		TEXTURE_MAP.try_emplace( "Normal Map",	CreateSingleTexelTexture( std::array< unsigned char, 4 >{ 127, 127, 255, 255 }, "Default Normal Map",
+																		  Texture::ImportSettings
+																		  {
+																			  .wrap_u = Texture::Wrapping::ClampToEdge,
+																			  .wrap_v = Texture::Wrapping::ClampToEdge,
+																			  .min_filter = Texture::Filtering::Nearest,
+																			  .mag_filter = Texture::Filtering::Nearest,
+																			  .flip_vertically = false,
+																			  .generate_mipmaps = false,
+
+																			  .format = Texture::Format::RGBA
+																		  } ) );
+
 
 		TEXTURE_MAP.try_emplace( "Missing", AssetDatabase< Texture >::CreateAssetFromFile( "Missing", FullTexturePath( "missing_texture.jpg" ),
 																						   Texture::ImportSettings
--- Engine/Engine/Graphics/InternalTextures.h
+++ Engine/Engine/Graphics/InternalTextures.h
@@ -19,6 +19,11 @@ namespace Engine
 	public:
 		static Texture* Get( const std::string& name );
 
+		static Texture* CreateSingleTexelTexture( const Color4& texel, const std::string& name, 
+												  std::optional< Texture::ImportSettings > import_settings = std::nullopt );
+		static Texture* CreateSingleTexelTexture( const std::array< unsigned char, 4 > texel, const std::string& name,
+												  std::optional< Texture::ImportSettings > import_settings = std::nullopt );
+
 	private:
 		static void Initialize();
 
