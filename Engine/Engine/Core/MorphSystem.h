f81fc88a9705d992bacef5f42ffdbedd59a463ff
fauder <canik.burak@gmail.com>
2025-07-09 17:20:44 +0300

    Engine/Core/Morph System: Split code into cpp module

--- Engine/Engine/Core/MorphSystem.h
+++ Engine/Engine/Core/MorphSystem.h
@@ -5,7 +5,6 @@
 #include "Morph.h"
 
 // std Includes.
-#include <functional>
 #include <vector>
 
 namespace Engine
@@ -24,41 +23,8 @@ namespace Engine
 		 * Usage: 
 		 */
 
-		void Add( Morph&& new_morph )
-		{
-			new_morph.remaining_duration_in_seconds = new_morph.duration_in_seconds;
-			morph_array.emplace_back( std::move( new_morph ) );
-		}
-
-		void Execute( const float delta_time_in_seconds )
-		{
-			// Use erase-remove idiom to remove finished Morphs.
-			auto remove_iterator = morph_array.end();
-
-			for( auto& morph : morph_array )
-			{
-				if( morph.remaining_duration_in_seconds < delta_time_in_seconds )
-				{
-					const auto t = ( morph.duration_in_seconds - morph.remaining_duration_in_seconds ) / morph.duration_in_seconds;
-
-					morph.on_execute( t );
-					morph.on_complete();
-
-					remove_iterator = std::remove( morph_array.begin(), morph_array.end(), morph );
-				}
-				else
-				{
-					const auto t = ( morph.duration_in_seconds - ( morph.remaining_duration_in_seconds - delta_time_in_seconds ) ) / morph.duration_in_seconds;
-
-					morph.on_execute( t );
-
-					morph.remaining_duration_in_seconds -= delta_time_in_seconds;
-				}
-			}
-
-			if( remove_iterator != morph_array.end() )
-				morph_array.erase( remove_iterator, morph_array.end() );
-		}
+		void Add( Morph&& new_morph );
+		void Execute( const float delta_time_in_seconds );
 
 	private:
 		std::vector< Morph > morph_array;
