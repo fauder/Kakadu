6d9c0c478e66a8195b6c5aebfc402fcded92b740
fauder <canik.burak@gmail.com>
2025-09-21 02:10:56 +0300

    Engine/Graphics/Shader & ShaderIncludePreprocessing: Add #line support for pre-processing + add file paths to error logs

--- Engine/Engine/Graphics/ShaderIncludePreprocessing.h
+++ Engine/Engine/Graphics/ShaderIncludePreprocessing.h
@@ -2,11 +2,13 @@
 
 // std Includes.
 #include <filesystem>
+#include <span>
 #include <unordered_map>
 
 namespace Engine::ShaderIncludePreprocessing
 {
 	std::string Resolve( const std::filesystem::path& source_path, 
 						 std::initializer_list< std::string > include_directories_with_trailing_slashes,
-						 std::unordered_map< std::int16_t, std::filesystem::path >& map_of_IDs_per_source_file );
+						 std::unordered_map< std::int16_t, std::filesystem::path >& map_of_IDs_per_source_file,
+						 std::span< char > error_log );
 }
