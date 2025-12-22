238bcb5396e1ac6ec75b3a9bb5058e5e84d57b7a
fauder <canik.burak@gmail.com>
2025-12-22 18:43:11 +0300

    Engine/Math/Concept: Add new concept Arithmetic_NotBool

--- Engine/Engine/Math/Concepts.h
+++ Engine/Engine/Math/Concepts.h
@@ -5,7 +5,8 @@
 
 namespace Engine::Concepts
 {
-	template< typename T > concept Arithmetic = std::integral< T > || std::floating_point< T >;
+	template< typename T > concept Arithmetic         = std::integral< T > || std::floating_point< T >;
+	template< typename T > concept Arithmetic_NotBool = Arithmetic< T > && not std::same_as< T, bool >;
 
 	template< std::size_t value > concept NonZero = ( value != 0 );
 
