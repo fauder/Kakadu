03b22bf8ed25556f0c169c2345ecaf3399bd13fd
fauder <canik.burak@gmail.com>
2025-12-25 13:59:32 +0300

    Engine/Core/Concepts: Add new concepts Pointer and NotPointer

--- Engine/Engine/Math/Concepts.h
+++ Engine/Engine/Math/Concepts.h
@@ -3,10 +3,15 @@
 // std Includes.
 #include <concepts>
 
+// TODO: Split into math concepts and non-math concepts.
+
 namespace Engine::Concepts
 {
 	template< typename T > concept Arithmetic         = std::integral< T > || std::floating_point< T >;
 	template< typename T > concept Arithmetic_NotBool = Arithmetic< T > && not std::same_as< T, bool >;
+
+	template< typename T > concept Pointer    = std::is_pointer_v< T >;
+	template< typename T > concept NotPointer = not Pointer< T >;
 
 	template< std::size_t value > concept NonZero = ( value != 0 );
 
