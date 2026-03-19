134c619dc16d5efdd16e18fe83ff30b3ff0e4c4f
fauder <canik.burak@gmail.com>
2026-03-19 17:57:08 +0300

    Engine/Core/Optimization: Also assert on debug builds

--- Engine/Engine/Core/Optimization.h
+++ Engine/Engine/Core/Optimization.h
@@ -1,9 +1,21 @@
 #pragma once
+// Engine Includes.
+#include "Assertion.h"
 
-#if defined( _MSC_VER )
-#define UNREACHABLE() __assume( 0 )
-#elif defined( __GNUC__ ) || defined( __clang__ )
-#define UNREACHABLE() __builtin_unreachable()
+#ifdef _DEBUG
+    #ifdef _MSC_VER
+        #define UNREACHABLE() ASSERT( "SHOULD HAVE BEEN UNREACHABLE." ); __assume( 0 )
+    #elif defined( __GNUC__ ) || defined( __clang__ )
+        #define UNREACHABLE() ASSERT( "SHOULD HAVE BEEN UNREACHABLE." ); __builtin_unreachable()
+    #else
+        #define UNREACHABLE() ASSERT( "SHOULD HAVE BEEN UNREACHABLE." )
+    #endif
 #else
-#define UNREACHABLE()
+    #ifdef _MSC_VER
+        #define UNREACHABLE() __assume( 0 )
+    #elif defined( __GNUC__ ) || defined( __clang__ )
+        #define UNREACHABLE() __builtin_unreachable()
+    #else
+        #define UNREACHABLE()
+    #endif
 #endif
